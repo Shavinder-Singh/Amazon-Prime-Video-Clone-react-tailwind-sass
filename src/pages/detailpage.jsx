0ec5f1cf0fce9753c66c89a306ee528bc8432042
Shavinder <singhshub3004@gmail.com>
2024-04-05 16:07:27 +0530

    updating

--- src/pages/detailpage.jsx
+++ src/pages/detailpage.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Detailmain from '../components/detailpage/detailmain/detailmain'
-import Detail_info from '../components/detailpage/detail_Info/detail_info'
 const detailpage = () => {
     return (
         <div>
             <Detailmain />
-            <Detail_info />
         </div>
     )
 }
